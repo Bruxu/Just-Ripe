dcf43866040ef301cb48c0a7be5a72b747f046ea
Moriah_Obaje <mobaje20@gmail.com>
2018-11-16 08:26:31 +0000

    Database Update

---
+++ dbo.Crop.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Crop] (
+    [CropName]    VARCHAR (20) NOT NULL,
+    [Price]       FLOAT(3)         NULL,
+    [Quantity]    INT          NULL,
+    [Fertilizer ] VARCHAR (20) NULL,
+    PRIMARY KEY CLUSTERED ([CropName] ASC)
+);
+
+
+GO
