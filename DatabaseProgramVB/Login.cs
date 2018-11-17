95fc4721f9fdf4265e18f4c17fbbce38dcf506d6
Hedjelk Cravid <hedjelk@hotmail.com>
2018-11-17 09:02:07 +0000

    login code polished

--- DatabaseProgramVB/Login.cs
+++ DatabaseProgramVB/Login.cs
@@ -20,19 +20,55 @@ namespace DatabaseProgramVB
         string[] Usernames = { "Manager", "Labourer" }; // users 
         string[] Passwords = { "Manager1", "Labourer1" };  // passwords for the users
 
-        private void button1_Click(object sender, EventArgs e)
+        private void TryLogin()
         {
             //condition for the next form appear 
             if (Usernames.Contains(textBox1.Text) && Passwords.Contains(textBox2.Text) &&
-                Array.IndexOf(Usernames, textBox1.Text) == Array.IndexOf(Passwords, textBox2.Text
-                ))
+               Array.IndexOf(Usernames, textBox1.Text) == Array.IndexOf(Passwords, textBox2.Text
+               ))
+
             {
-                EntityForm f2 = new EntityForm(); // a new form is open by a click on the button
-                Hide();
-            f2.ShowDialog();
+                Hide();//Hide current form
+                EntityForm f2 = new EntityForm() // a new form is open by a click on the button
+                                        //Add closing event to the entity object f2
+
+                f2.FormClosing += delegate
+                {
+                    Close();//close current form  
+                };
+                f2.ShowDialog();
             }
             else
                 MessageBox.Show("The Username or Password is incorrect, please try again");
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            TryLogin();
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            //if down arrow key is pressed change focus to password box
+            if (e.KeyCode == Keys.Down)
+            {
+                TextBox1.Focus();
+            }
+        }
+
+        private void TextBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            //Run login method if the enter key is pressed
+            if (e.KeyCode == Keys.Enter)
+            {
+                TryLogin();
+            }
+            if (e.KeyCode == Keys.Up)
+            {
+                textBox1.Focus();
+            }
+        }
+
+
     }
-    }
+}
